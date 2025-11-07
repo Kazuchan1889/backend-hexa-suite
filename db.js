135aad064bc657f5ae0303e3e841b6cd028460bc
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-11-07 10:07:15 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
-import pkg from "pg";
-const { Pool } = pkg;
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
-export default pool;
+module.exports = pool;
